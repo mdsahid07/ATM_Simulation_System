94ccfe250452ee6791d5efe637c1f877f79bd714
mdsah <mdsahidhossain.miu@gmail.com>
2024-10-21 15:59:48 -0500

    connect to the database

---
+++ ATM_Simulation_System/src/Data_Access/MainDAL.java
@@ -0,0 +1,24 @@
+package Data_Access;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.*;
+
+public class MainDAL {
+    public static void main(String[] args) {
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/atmsystem", "root", "123456");
+            Statement statement = con.createStatement();
+            ResultSet query = statement.executeQuery("Select * from User");
+
+            while (query.next()) {
+                System.out.println(query.getString("name"));
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
