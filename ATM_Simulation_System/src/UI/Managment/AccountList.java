1ddef2217e0e393acfe817a1f1d38b26f51a2da2
Shiqi <450008475@qq.com>
2024-10-22 19:09:03 -0500

    update

--- ATM_Simulation_System/src/UI/Managment/AccountList.java
+++ ATM_Simulation_System/src/UI/Managment/AccountList.java
@@ -7,19 +7,22 @@ public class AccountList extends JFrame {
     private JTable table1;
     private JButton searchButton;
     private JTextField textField1;
+    private JPanel panel1;
     private JButton deleteButton;
 
     public AccountList() {
+        setContentPane(panel1);
         // Default visibility is false. You have enabled visibility true
         setVisible(true);
         // Terminates the Application when the frame is closed.
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setTitle("Login Form");
         // Provide the frame width and height
-        setSize(300, 400);
+        setSize(500, 500);
         // Make your screen center
         setLocationRelativeTo(null);
         setResizable(false);// If you wish
+
     }
     public static void main(String[] args) {
  /*
